99f7ad57afb3da256fdd822c84bc4f9a8355be80
julio.rugolo <juliorugolo@gmail.com>
2024-08-06 13:10:36 -0300

    add loader popup

--- src/components/PhotoPopup/PhotoPopup.jsx
+++ src/components/PhotoPopup/PhotoPopup.jsx
@@ -1,15 +1,32 @@
 // src/components/PhotoPopup/PhotoPopup.jsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './PhotoPopup.css';
 import logo from '../../assets/img/festival.png'; // Importe o logo
 
 const PhotoPopup = ({ photo, onClose, onNext, onPrev }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+  }, [photo]);
+
+  const handleImageLoad = () => {
+    setIsLoading(false);
+  };
+
   return (
     <div className='photo-popup'>
       <div className='photo-popup-content'>
         <span className='close' onClick={onClose}>&times;</span>
         <div className='photo-container'>
-          <img src={photo} alt="Foto Ampliada" className='photo' />
+          {isLoading && <div className='loader'></div>}
+          <img
+            src={photo}
+            alt="Foto Ampliada"
+            className='photo'
+            style={{ display: isLoading ? 'none' : 'block' }}
+            onLoad={handleImageLoad}
+          />
           <img src={logo} alt="Logo" className='logo' />
         </div>
         <button onClick={onPrev} className='pop-up-button prev-button'>&#8249;</button>
